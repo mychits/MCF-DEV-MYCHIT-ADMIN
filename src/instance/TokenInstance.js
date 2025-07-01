10da56b82e0115fa4bb8c5f8e979333a985bd2df
jeev-creator <aruvajeevitha@gmail.com>
2025-07-01 15:50:38 +0530

    elastic ip added 13.61.139.208

--- src/instance/TokenInstance.js
+++ src/instance/TokenInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const token = localStorage.getItem("Token");
 const api = axios.create({
-    //baseURL: "http://51.21.197.152:3000/api",
-    baseUrl: "https://mychits.online/api",
+    // baseUrl: "https://mychits.online/api",
+    baseUrl: "http://13.61.139.208:3000/api",
+    
       //baseURL: "http://localhost:3000/api",
   headers: {
     Authorization: `Bearer ${token}`,
