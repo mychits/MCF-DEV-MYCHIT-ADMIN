adb8d53bf69bea3c0540d1267cdd9b3dd7e6c52b
grash2024 <grash2024@gmail.com>
2026-02-02 13:25:08 +0530

    baseurl changed

--- src/instance/BaseUrl.js
+++ src/instance/BaseUrl.js
@@ -1,2 +1,2 @@
- const BaseURL = "http://13.48.115.111:3000/api"; //"http://localhost:3000/api"
+ const BaseURL = "http://51.21.197.152:3000/api"; //"http://localhost:3000/api"
  export default  BaseURL;
